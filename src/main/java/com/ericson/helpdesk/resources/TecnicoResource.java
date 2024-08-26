e1fe0a347814e3c4b314e4dd4ad2bc82419cabee
PSICOTICO <PSICOTICO@192.168.0.7>
2024-08-26 19:50:46 -0300

    Endpoint findAll para Tecnico

--- src/main/java/com/ericson/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/ericson/helpdesk/resources/TecnicoResource.java
@@ -1,12 +1,13 @@
 package com.ericson.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.ericson.helpdesk.domain.Tecnico;
 import com.ericson.helpdesk.dtos.TecnicoDTO;
 import com.ericson.helpdesk.services.TecnicoService;
@@ -14,17 +15,27 @@ import com.ericson.helpdesk.services.TecnicoService;
 @RestController
 @RequestMapping("/tecnicos")
 public class TecnicoResource {
-	
+
 	@Autowired
 	private TecnicoService tecnicoService;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
-		
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
+
 		Tecnico obj = tecnicoService.findById(id);
 		TecnicoDTO tecnicoDTO = new TecnicoDTO(obj);
-		
+
 		return ResponseEntity.ok().body(tecnicoDTO);
-		
+
+	}
+
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+
+		List<Tecnico> list = tecnicoService.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDTO);
+
 	}
 }
